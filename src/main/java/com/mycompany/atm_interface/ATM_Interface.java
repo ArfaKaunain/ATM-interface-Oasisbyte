ab7920c6fd451008f0c0a01d171f71cb4343fa80
arfa <arfakaunain51846@gmail.com>
2024-09-18 15:36:50 +0530

    Initial commit

---
+++ src/main/java/com/mycompany/atm_interface/ATM_Interface.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.atm_interface;
+
+/**
+ *
+ * @author arfa kaunain
+ */
+public class ATM_Interface {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
